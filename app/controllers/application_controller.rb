c3193d4d7da83a9c623496badaa4243e071212a4
Parham Javadi <pjfooeve09@yahoo.com>
2021-03-08 10:09:41 -0800

    commit tree 4efef4b9e507ef4a98b1d645f453721c22db1a7c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -5,6 +6,75 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :home
+  end
+
+  get '/signup' do
+    if session[:user_id]
+      redirect '/tweets'
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect '/signup'
+    else
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      #need helper method isLoggedIn?
+      redirect '/tweets'
+    end
+  end
+
+  get '/login' do
+    if session[:user_id]
+      redirect '/tweets'
+    else
+      "This is the login page"
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    if session[:user_id]
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
+
+  post '/logout' do
+    if session[:user_id]
+      redirect '/tweets'
+    end
   end
 
 end
+
+
+
+
+
+
+
+
+
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
 
-
-end
+    get '/users' do
+      if session[:user_id]
+        @users = User.all
+        erb :'users/users'
+      else
+        redirect '/login'
+      end
+    end
+  
+    #route to get user page by id
+    get '/users/:slug' do
+      @user = User.find_by_slug(params[:slug])
+      erb :'users/user'
+    end
+  
+  end
